355e907ed4fc4c420014905bc4ca20fd4776e8dc
Jay Shapiro <jas2dp@virginia.edu>
2017-08-17 19:53:35 +0000

    FINALLY FIXED THE PROBLEM I SPENT THE LAST THREE WEEKS ON HOLY SHIT

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   # and save the user.
   def save_with_subscription
     if valid?
-      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      customer = Stripe::Customer.create(description: email, plan: plan_id, source: stripe_card_token)
       self.stripe_customer_token = customer.id
       save!
     end
