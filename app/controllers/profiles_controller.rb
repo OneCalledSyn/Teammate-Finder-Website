341df39cd411c3b640e920ab9a59d1381854b8ba
Jay Shapiro <jas2dp@virginia.edu>
2017-10-25 21:31:46 +0000

    Added profile edit action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -20,6 +20,12 @@ class ProfilesController < ApplicationController
     end
   end
   
+  # GET to /users/:user_id/profile/edit
+  def edit
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+  end
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :summoner_name, :main_role, :secondary_role, :solo_rank, :flex_rank, :phone_number, :email_address, :description)
